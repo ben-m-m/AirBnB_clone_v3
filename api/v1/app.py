c106b8b00c9e3ace638bec1023e314404ffe50cf
ben-m-m <mainaben23@gmail.com>
2023-07-31 10:05:06 +0300

    documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""flask app - model"""
 from os import environ
 from flask import Flask
 from models import storage
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""index file"""
 from api.v1.views
 from flask import jsonify
 
