0272f2abac86023efdfc80056c8b86d1ed9647dc
Dennis Muvaa <dmuvaa70@gmail.com>
2023-08-01 19:28:10 +0300

    Updates

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
-"""index file"""
-from api.v1.views import app_views
-from flask import jsonify
 
+"""index file"""
+
+from api.v1.views import app_views
+from flask import jsonifye
+from models import storage
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
+    """return status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """return stats"""
+    classes = ["Amenity", "City", "Place", "Review", "State", "User"]
+    counts = {}
+    for cls in classes:
+        counts[cls.lower()] = storage.count(cls)
+    return jsonify(counts)
