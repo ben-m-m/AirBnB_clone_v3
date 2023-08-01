5781b3c4ec66c4384678ee765bcf94bdd599d9f2
Dennis Muvaa <dmuvaa70@gmail.com>
2023-08-02 00:49:32 +0300

    Index File updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,9 +12,9 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-
-classes = {"Amenity": Amenity, "City": City, "Place": Place, 
+classes = {"Amenity": Amenity, "City": City, "Place": Place,
            "Review": Review, "State": State, "User": User}
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
@@ -26,13 +26,14 @@ def status():
 def count_objects():
     """ count the number of objects in each class"""
     object_names = {
-        'states': 'State', 
-        'cities': 'City', 
-        'amenities': 'Amenity', 
-        'places': 'Place', 
-        'reviews': 'Review', 
-        'users': 'User'
+            'states': 'State',
+            'cities': 'City',
+            'amenities': 'Amenity',
+            'places': 'Place',
+            'reviews': 'Review',
+            'users': 'User'
     }
 
-    objects = {name: storage.count(classes[cls_name]) for name, cls_name in object_names.items()}
+    objects = {name: storage.count(classes[cls_name]) for name,
+               cls_name in object_names.items()}
     return jsonify(objects)
